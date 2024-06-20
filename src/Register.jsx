79365c699a2a714be79c4f375614153420c8a54d
GilbertImmanuel <gilbertimmanuel13@gmail.com>
2024-06-20 18:38:40 +0700

    feat: fixed Register

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,22 +1,54 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import googleIcon from './assets/googleIcon.png';
-import logo from './assets/logo.png'; // Add the path to your Google icon image
+import logo from './assets/logo.png';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Register = () => {
   const navigate = useNavigate();
 
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const handleRegister = async () => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
 
-  const handleSubmit = (e) => {
+    const requestBody = {
+      email: document.getElementById('email').value,
+      password: document.getElementById('password').value,
+      passwordConfirm: document.getElementById('passwordConfirm').value
+    };
+
+    if (!requestBody.email || !requestBody.password || !requestBody.passwordConfirm) {
+      toast.error('All fields are required');
+      return;
+    }
+
+    if (requestBody.password !== requestBody.passwordConfirm) {
+      toast.error('Passwords do not match');
+      return;
+    }
+
+    const response = await axios.post(import.meta.env.VITE_REACT_APP_BACKEND_URL + '/api/v1/auth/email/register', requestBody, config).catch((error) => {
+      switch (error.response.data.status) {
+        case 'error':
+          toast.error((error.response.data.message) ? error.response.data.message : (error.response.statusText) ? error.response.statusText : 'An unexpected error occurred');
+          break;
+        default:
+      }
+    });
+
+    if (response && (response.status === 204 || (response.data && response.data.success))) {
+      toast.success('Registration successful');
+      navigate('/login');
+    }
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Registration logic here
-    console.log('Register with:', fullName, email, password, confirmPassword);
-    // Navigate to the login page
-    navigate('/login');
+    await handleRegister();
   };
 
   return (
@@ -26,19 +58,6 @@ const Register = () => {
         <div className="w-full max-w-md">
           <form className="mt-8 space-y-6 bg-white shadow-md rounded-xl px-8 pt-6 pb-8" onSubmit={handleSubmit}>
             <div className="rounded-md shadow-sm -space-y-px">
-              {/* Full Name Input */}
-              <div className="mb-3">
-                <label className="block text-gray-700 text-sm font-semibold mb-2" htmlFor="full-name">
-                  Full name
-                </label>
-                <input
-                  id="full-name"
-                  type="text"
-                  value={fullName}
-                  onChange={(e) => setFullName(e.target.value)}
-                  className="appearance-none rounded-md relative block w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                />
-              </div>
               {/* Email Input */}
               <div className="mb-3">
                 <label className="block text-gray-700 text-sm font-semibold mb-2" htmlFor="email">
@@ -47,8 +66,6 @@ const Register = () => {
                 <input
                   id="email"
                   type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
                   className="appearance-none rounded-md relative block w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm mb-3"
                 />
               </div>
@@ -60,8 +77,6 @@ const Register = () => {
                 <input
                   id="password"
                   type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
                   className="appearance-none rounded-md relative block w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm mb-3"
                 />
               </div>
@@ -71,16 +86,18 @@ const Register = () => {
                   Confirm Password
                 </label>
                 <input
-                  id="confirm-password"
+                  id="passwordConfirm"
                   type="password"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                  className="appearance-none rounded-md relative block w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" />
+                  className="appearance-none rounded-md relative block w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                />
               </div>
             </div>
             {/* Register Button */}
             <div className="flex items-center justify-between mb-3">
-              <button type="submit" className="w-full flex justify-center py-3 px-4 border border-transparent text-sm font-bold rounded-2xl text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <button
+                type="submit"
+                className="w-full flex justify-center py-3 px-4 border border-transparent text-sm font-bold rounded-2xl text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
                 Register
               </button>
             </div>
@@ -91,14 +108,17 @@ const Register = () => {
             </div>
             {/* Google Register Button */}
             <div className="flex items-center justify-center mb-6">
-              <button type="button" className="group w-full flex justify-center py-3 px-4 border border-gray-300 text-sm font-medium rounded-2xl text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <button
+                type="button"
+                className="group w-full flex justify-center py-3 px-4 border border-gray-300 text-sm font-medium rounded-2xl text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
                 <img src={googleIcon} alt="Google sign-in" className="h-6 w-6 mr-3" />
                 Register with Google
               </button>
             </div>
             <div className="text-center">
               <p className="text-gray-600 text-sm">
-                Have an account? {' '}
+                Have an account?{' '}
                 <Link to="/login" className="font-semibold text-gray-700 hover:text-gray-800 cursor-pointer">
                   Log In Now
                 </Link>
